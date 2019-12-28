8eeb056323b8d706262ef162f27e84d2163e0a13
XiaochenCui <jcnlcxc.new@gmail.com>
2019-12-28 21:14:02 +0800

    Remove target folder

--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/xiaochen/project/rust-playground/guessing_game/target/debug/guessing_game: /Users/xiaochen/project/rust-playground/guessing_game/src/main.rs
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/xiaochen/project/rust-playground/hello_cargo/target/release/hello_cargo: /Users/xiaochen/project/rust-playground/hello_cargo/src/main.rs
